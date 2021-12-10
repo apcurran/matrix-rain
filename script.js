a407014e5fa2182bcff3b7c264a2683f00484db3
Alex Curran <alexcurran.va@gmail.com>
2021-12-10 11:01:07 -0600

    Base class setup

--- script.js
+++ script.js
@@ -4,10 +4,38 @@
 const canvas = document.getElementById("canvas");
 /** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext("2d");
+// Initial canvas sizing based on viewport
+canvas.width = document.documentElement.clientWidth;
+canvas.height = document.documentElement.clientHeight;
 
+class Symbol {
+    constructor() {
 
+    }
 
+    draw() {
 
+    }
+}
+
+class RainEffect {
+    constructor(canvasWidth, canvasHeight) {
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+        this.fontSize = 25;
+        this.columns = this.canvasWidth / this.fontSize;
+        this.symbols = [];
+    }
+    // Private method
+    #init() {
+
+    }
+    // Public method
+}
+
+function animate() {
+
+}
 
 
 
