b18647509fe381b2e0e473272e5b136e84379a69
Alex Curran <alexcurran.va@gmail.com>
2021-12-10 10:12:47 -0600

    Resize event listener and func

--- script.js
+++ script.js
@@ -4,3 +4,25 @@
 const canvas = document.getElementById("canvas");
 /** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext("2d");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.addEventListener("resize", resizeCanvas);
+
+function resizeCanvas(canvas) {
+    canvas.width = document.documentElement.clientWidth;
+    canvas.height = document.documentElement.clientHeight;
+}
