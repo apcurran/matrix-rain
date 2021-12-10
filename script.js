04650e3c95ac007ce4bd35962df8fe05d6982b3c
Alex Curran <alexcurran.va@gmail.com>
2021-12-10 11:19:58 -0600

    Animate working with ES6 classes

--- script.js
+++ script.js
@@ -8,13 +8,26 @@ const ctx = canvas.getContext("2d");
 canvas.width = document.documentElement.clientWidth;
 canvas.height = document.documentElement.clientHeight;
 
-class Symbol {
-    constructor() {
-
+class MatrixSymbol {
+    constructor(x, y, fontSize, canvasHeight) {
+        this.characters = "アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        this.x = x;
+        this.y = y;
+        this.fontSize = fontSize;
+        this.canvasHeight = canvasHeight;
+        this.text = "";
     }
 
-    draw() {
+    draw(context) {
+        this.text = this.characters.charAt(getRandomInt(this.characters.length));
+        context.fillStyle = "#0aff0a";
+        context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
 
+        if (this.y * this.fontSize > this.canvasHeight) {
+            this.y = 0;
+        } else {
+            this.y += 1;
+        }
     }
 }
 
@@ -25,16 +38,41 @@ class RainEffect {
         this.fontSize = 25;
         this.columns = this.canvasWidth / this.fontSize;
         this.symbols = [];
+        this.#init();
     }
     // Private method
     #init() {
-
+        for (let i = 0; i < this.columns; i++) {
+            this.symbols[i] = new MatrixSymbol(i, 0, this.fontSize, this.canvasHeight);
+        }
     }
     // Public method
 }
 
-function animate() {
+const effect = new RainEffect(canvas.width, canvas.height);
 
+function animate() {
+    ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.font = `${effect.fontSize}px monospace`;
+
+    for (let symbol of effect.symbols) {
+        symbol.draw(ctx);
+    }
+
+    requestAnimationFrame(animate);
+}
+
+// Kick off
+animate();
+
+// Util func
+/**
+ * @param {number} charsLen 
+ * @returns {number}
+ */
+function getRandomInt(charsLen) {
+    return Math.floor(Math.random() * charsLen);
 }
 
 
